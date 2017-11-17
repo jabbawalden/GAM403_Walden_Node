f9c9c34879bd07af7c99ad6fe5c224d9b697ec29
jabbawalden <33333251+jabbawalden@users.noreply.github.com>
2017-11-17 11:04:39 +1100

    Code work
    working on finding my own way of moving an object between two positions.
    Keeping the code simple.
    Unfortunately position is offset from P1 and P2, so even though it moves, it's position is never actually equal to them. This is problematic for getting it to move back to P1 after reaching P2

---
+++ Assets/OMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OMovement : MonoBehaviour {
+
+
+
+    public Transform P1;
+    public Transform P2;
+    bool moveRight;
+    bool moveLeft;
+    float speed = 2;
+
+    private void Start()
+    {
+        transform.position = P1.transform.position;
+        moveRight = true;
+        moveLeft = false;
+    }
+
+    void Update()
+    {
+        float eSpeed = speed * Time.deltaTime;
+
+      if (moveRight == true)
+       transform.position = Vector2.MoveTowards(transform.position, P2.position, eSpeed);
+        
+      if (moveLeft == true)
+       transform.position = Vector2.MoveTowards(transform.position, P1.position, eSpeed);
+
+      if (transform.position == P1.position)
+        {
+            print("at position 1");
+        }
+
+     
+
+    }
+
+    /*
+     * 
+     * if (transform.position == P2.transform.position)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, P1.position, eSpeed);
+        }
+	// Use this for initialization
+	void Start ()
+    {
+        Vector2 Pos1 = new Vector2(P1.position.x, P1.position.y);
+        Vector2 Pos2 = new Vector2(P2.position.x, P2.position.y);
+        transform.position = Pos1;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+		if (transform.position == P1.transform.position)
+        {
+            Vector2.MoveTowards (transform.position, P2.transform.position, Speed * Time.deltaTime);
+        }
+
+	}
+    */
+}
