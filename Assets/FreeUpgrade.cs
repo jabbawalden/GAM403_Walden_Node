2396eab43efba967b480f7e8ddd5395f682dab93
jabbawalden <33333251+jabbawalden@users.noreply.github.com>
2017-12-05 20:46:15 +1100

    Minor Changes
    committing regularly just incase I lose the project somehow... sorry Joe for the millions of emails.

--- Assets/FreeUpgrade.cs
+++ Assets/FreeUpgrade.cs
@@ -14,6 +14,7 @@ public class FreeUpgrade : MonoBehaviour {
         if (GameController.control.freeUpgrade == false)
         {
             Destroy(freeUpgradeBox);
+            Destroy(colliderCheck);
         }
     }
 
